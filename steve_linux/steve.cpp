8ee262e806fdbe92ce01103cdb6ffabab5aae644
danjoannis <danjoannis@gmail.com>
2015-12-12 15:27:20 -0330

    Initial commit of Linux application

---
+++ steve_linux/steve.cpp
@@ -0,0 +1,16 @@
+/*
+ * steve.cpp
+ *
+ *  Created on: Dec 12, 2015
+ *      Author: Daniel
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello world!" << endl;
+
+	return 0;
+}
