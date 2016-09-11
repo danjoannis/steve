84e479acfbb61799321b897a812663020713e971
danjoannis <danjoannis@gmail.com>
2016-09-11 16:29:18 -0230

    Added initial python webui files

---
+++ steve_py/steve_web.py
@@ -0,0 +1,17 @@
+import os, time
+from bottle import route, run, template
+
+def cpu_temp():
+  dev = os.popen('/opt/vc/bin/vcgencmd measure_temp')
+  cpu_temp = dev.read()[5:-3]
+  return cpu_temp
+
+@route('/cputemp')
+def temp():
+  return cpu_temp()
+
+@route('/')
+def index():
+  return template('main.html')
+
+run(host='0.0.0.0', port=80)
